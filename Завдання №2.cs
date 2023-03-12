f3407f994d70a6f1195612c2bae2ad3ed48e5aab
borderlands16 <49939719+borderlands16@users.noreply.github.com>
2023-03-12 23:02:17 +0530

    Add files via upload

---
+++ Завдання №2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Timer
+{
+    private int interval;
+    private Action action;
+    private System.Threading.Timer timer;
+
+    public Timer(int interval, Action action)
+    {
+        this.interval = interval;
+        this.action = action;
+    }
+
+    public void Start()
+    {
+        timer = new System.Threading.Timer(state =>
+        {
+            action();
+        }, null, 0, interval * 1000);
+    }
+
+    public void Stop()
+    {
+        timer.Dispose();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Timer timer = new Timer(1, () => Console.WriteLine(DateTime.Now.ToString("mm:ss")));
+
+        timer.Start();
+
+        Console.ReadLine();
+
+        timer.Stop();
+
+        Console.WriteLine("Timer stopped.");
+    }
+}
